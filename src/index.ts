369b85160ee7604245e7faede38b33d0e142b631
kyoya0819 <kyoya0819@gmail.com>
2021-08-14 04:50:17 +0900

    Splitting the source code

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,31 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-
-/**
- * Function to check if a user can be assigned.
- *
- * @param user
- * @param token
- */
-const check = async (user: string, token: string) => {
-
-    const octokit = github.getOctokit(token);
-
-    return octokit.rest.issues.checkUserCanBeAssigned({
-        ...github.context.repo,
-        assignee: user,
-    });
-};
+import check from "./scripts/check";
+import kill from "./scripts/kill";
+import { users, token, pull_request } from "./scripts/variables";
 
 
-/**
- * Create a new array by extracting a specific number of arrays.
- *
- * @param array
- * @param num
- */
-const extractArray = (array: string[], num: number) => {
-    const newArray: string[] = [];
-
-    while (newArray.length < num && 0 < array.length) {
-        const rand = Math.floor(Math.random() * array.length);
-        newArray.push(array[rand]);
-        array.splice(rand, 1);
-    }
-
-    return newArray;
-};
-
-
-const token = core.getInput("token");
-const input_users = JSON.parse(core.getInput("users"));
-let count = Number(core.getInput("count"));
-
-if (count === 0)
-    count = input_users.length;
-
-if (count > input_users.length) {
-
-    core.setFailed("The number of assignees is larger than the number of users specified.");
-    process.exit(1);
-}
-
-Promise.all(input_users.map(async user => {
-    return await check(user, token);
+Promise.all(users().map(async user => {
+    return await check(user, token());
 })).then(() => {
     core.info("All specified users can be assigned.");
 
-    const users = extractArray(input_users, count);
-
-    const octokit = github.getOctokit(token);
-
-    const { pull_request } = github.context.payload;
-
-    if (pull_request === undefined) {
-        core.setFailed("Unable to retrieve information about the pull request.");
-        process.exit(1);
-    }
-
-    octokit.rest.issues.addAssignees({
+    github.getOctokit(token()).rest.issues.addAssignees({
         ...github.context.repo,
-        issue_number: pull_request.number,
-        assignees: users
+        issue_number: pull_request(),
+        assignees: users(true)
     }).then(() => {
 
         core.info("Complete This Action ✨");
         process.exit(0);
     }).catch(() => {
 
-        core.setFailed("Cannot be assigned.");
-        process.exit(1);
+        kill("Cannot be assigned.");
     });
 
 }).catch((data) => {
 
     const user = data.response.url.match(/[^/]*?$/)[0];
-    core.setFailed(`"${ user }" cannot be assigned.`);
+    kill(`"${ user }" cannot be assigned.`);
 });
